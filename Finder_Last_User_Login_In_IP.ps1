124d9d4dd7792af7a0b4885a31eb8e186eef6a40
fmanakhov <52257415+fmanakhov@users.noreply.github.com>
2024-02-17 17:43:56 +0600

    First version

---
+++ Finder_Last_User_Login_In_IP.ps1
@@ -0,0 +1,14 @@
+﻿Write-Host "Enter username:"
+$Name = Read-Host
+
+$result = Get-EventLog -LogName Security -InstanceId 4624 -Newest 16 -Message *$Name* |
+   ForEach-Object {
+     [PSCustomObject]@{
+     Time = $_.TimeGenerated
+     IP = $_.ReplacementStrings[18]
+     }
+   }
+
+
+Write-Host "Last login of Username:" $Name 
+$result | Select-Object Time, IP
